9e070d5b92252ba2c3190621a2dc97eece9a7f94
aymen-moh <aymen.mohammed.abdl@gmail.com>
2026-09-25 16:41:28 +0300

    added saw blades and the ability to add multiple of them.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <raylib.h>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <raymath.h>
+struct MovingSawblade {
 
-struct Sawblade1 {
-    float sb_radius = 40.0f;
-    Vector2 sb_position = {80.0f, 130.0f};
+    Vector2 sb_pos_a;
+    Vector2 sb_pos_b;
+    Vector2 sb_pos_current;
+    bool sb_direction;
+    float sb_speed;
+    float sb_radius = 10.0f;
+};
+
+
+struct SawBlade {
+    Vector2 sb_position;
+    float radius;
+    float rotation_speed; //making this will probably be complicated but not now :/
 };
 
 
@@ -17,6 +30,15 @@ struct Player {
 };
 
 int main(int argc, char* argv[]) {
+
+
+    std::vector<MovingSawblade> movingsawblades {
+        { {50.0f, 50.0f}, {100.0f, 00.0f}, {50.0f, 50.0f}, true, 180.0f, 20.0f},
+        { {50.0f, 50.0f}, {250.0f, 100.0f}, {50.0f, 50.0f}, true, 180.0f, 20.0f},
+    };
+
+
+
     int width = 600;
     int height = 400;
     std::string title = "Billy Ray V0.0.1"; //i was gonna include the version number in a variable but i am just gonna do it this way :p
@@ -25,23 +47,47 @@ int main(int argc, char* argv[]) {
 
     Player player;
     player.position = {width/2.0f, height/2.0f}; 
-    Sawblade1 sawblade1;
+    MovingSawblade movingsawblade;
     bool dead = false;
     while(WindowShouldClose() == false && dead == false){
         Rectangle p_rect = {player.position.x, player.position.y, player.p_width, player.p_height};
         float dt = GetFrameTime();
+        for(auto& saw : movingsawblades){
+            Vector2 target;
+            if(saw.sb_direction){
+                target = saw.sb_pos_b;
+            }
+            else{
+                target = saw.sb_pos_a;
+            }
+
+            saw.sb_pos_current = Vector2MoveTowards(saw.sb_pos_current, target, saw.sb_speed * dt);
+            if(Vector2Distance(saw.sb_pos_current, target) < 1.0f){ // needed some help from our friend gemini here, it gave me the idea of using vector2move and vector2distance
+                saw.sb_direction = !saw.sb_direction;
+            }
+
+            if(CheckCollisionCircleRec(saw.sb_pos_current, saw.sb_radius, p_rect)){
+                dead = true;
+            }
+
+
+
+        }
+        
+        
+        
         if (IsKeyDown(KEY_W)) player.position.y -= player.speed * dt;
         if (IsKeyDown(KEY_S)) player.position.y += player.speed * dt;
         if (IsKeyDown(KEY_A)) player.position.x -= player.speed * dt;
         if (IsKeyDown(KEY_D)) player.position.x += player.speed * dt;
-        if (CheckCollisionCircleRec(sawblade1.sb_position, sawblade1.sb_radius, p_rect)){
-            dead = true;
-        }
+        
         BeginDrawing();
         ClearBackground(DARKGRAY);
         
         DrawRectangleV( player.position, {player.p_width, player.p_height}, YELLOW);
-        DrawCircleV(sawblade1.sb_position, sawblade1.sb_radius, RED);
+        for(const auto& saw : movingsawblades){
+            DrawCircleV(saw.sb_pos_current, saw.sb_radius, RED);
+        }
         
 
 
